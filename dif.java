18e490fdfefddc828d2a980bfd9c1bb998756777
Raju Barde <rajub.forbearpro@gmail.com>
2023-02-16 14:00:57 +0530

    Modification in dif.java

--- dif.java
+++ dif.java
@@ -1,8 +1,6 @@
 public class dif{
     public static void main(String args[])
     {
-        int a=10,b=20,sum;
-        sum = a-b;
-        System.out.println("dif  sum is"+sum);
+       
     }
 }
